fbfb02d51e8e982d4911497a0a02e6d8f9921426
Cristiano-Medeiros-Camarinho <381118502017.1@senacrjedu.local>
2018-10-15 10:31:37 -0300

    Produtos Mockados

---
+++ src/config/api.config.ts
@@ -0,0 +1,10 @@
+import { Produto } from "../model/produto";
+
+export const PRODUTOS : Produto[] = [
+    {id : 1, nome : "Celular LG K10", preco : 799.00, imagem : "k10.jpg"},
+    {id : 1, nome : "Celular Motorola Moto x", preco : 800.00, imagem : "motox.jpg"},
+    {id : 1, nome : "Celular Samsung J7", preco : 500.00, imagem : "sam.jpg"},
+    {id : 1, nome : "Celular LG K4", preco : 500.00, imagem : "lg.jpg"},
+    {id : 1, nome : "Celular Motorola Moto e", preco : 600.00, imagem : "motoe.jpg"},
+    {id : 1, nome : "Celular Motorola One", preco : 799.00, imagem : "motoone.jpg"}
+]
